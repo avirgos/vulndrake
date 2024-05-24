1cff7d393c635dee2b8b2c0fe05808586f833706
Antoine Virgos <antoine@virgos.fr>
2024-05-24 19:47:29 +0200

    First use of gvm-cli

--- app/controller/ManualController.php
+++ app/controller/ManualController.php
@@ -6,9 +6,7 @@ class ManualController {
     public function executeScanner() {
         if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["execute"])) {
             $containerName = "vulndrake-worker";
-
             $command = "docker exec -u gvm-user $containerName sh worker/scanner.sh 2>&1";
-
             $output = shell_exec($command);
             echo "<pre>$output</pre>";
         }
--- worker/scanner.sh
+++ worker/scanner.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
 . worker/venv/bin/activate
+
+gvm-cli --gmp-username admin --gmp-password admin socket --xml "<get_version/>"

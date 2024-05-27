ab0473b23d104730722b0448f2b967d2a03af540
Antoine Virgos <antoine@virgos.fr>
2024-05-27 14:59:04 +0200

    config.cfg for gmp-username and gmp-password

--- worker/scanner.sh
+++ worker/scanner.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+CONFIG_FILE="worker/config.cfg"
+USERNAME=$(grep "username" "${CONFIG_FILE}" | cut -d" " -f3)
+PASSWORD=$(grep "password" "${CONFIG_FILE}" | cut -d" " -f3)
+
 . worker/venv/bin/activate
 
-gvm-cli --gmp-username admin --gmp-password admin socket --xml "<get_version/>"
+gvm-cli --gmp-username "${USERNAME}" --gmp-password "${PASSWORD}" socket --xml "<get_version/>"

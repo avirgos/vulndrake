a809ca102917f4ce978f06de0d84f3f7105ba657
Antoine Virgos <antoine@virgos.fr>
2024-06-22 21:08:36 +0200

    Favicon added

--- app/index.php
+++ app/index.php
@@ -4,6 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VulnDrake | Accueil</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="data/img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="data/img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="data/img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="data/img/favicon/site.webmanifest">
+    <link rel="mask-icon" href="data/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
     <script src="js/script.js"></script>
 </head>
 <body>
